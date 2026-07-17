e3358c952398cbeaeabcbfa74179d6d53521f316
Aayushkassey <surump3@gmail.com>
2026-07-17 12:50:33 +0545

    Jazzmin

--- waste_system/settings.py
+++ waste_system/settings.py
@@ -16,6 +16,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(','
 
 # ─── Apps ─────────────────────────────────────────────────────────────────────
 INSTALLED_APPS = [
+    'jazzmin',  # Admin theme
     'daphne',  # must be FIRST for ASGI
     'django.contrib.admin',
     'django.contrib.auth',
